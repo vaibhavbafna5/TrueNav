aad6f9afc8ee279fc4b331f2fea4ec3503442a92
Vaibhav Bafna <vbafna@umich.edu>
2017-11-06 14:34:24 -0500

    wrote a test version to see if we get status OK

--- app/src/main/java/com/example/vaibhav/truenav/MainActivity.java
+++ app/src/main/java/com/example/vaibhav/truenav/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.vaibhav.truenav;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.model.Direction;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView dummyTextView = (TextView) findViewById(R.id.dummy_textview);
+        String serverKey = "AIzaSyAlmHxG5gNxyrDhSGcrYolJA__x17rNlM0";
+        LatLng origin = new LatLng(37.7849569, -122.4068855);
+        LatLng destination = new LatLng(37.7814432, -122.4460177);
+        GoogleDirection.withServerKey(serverKey)
+                .from(origin)
+                .to(destination)
+                .execute(new DirectionCallback() {
+                    @Override
+                    public void onDirectionSuccess(Direction direction, String rawBody) {
+                        String status = direction.getStatus();
+                        dummyTextView.setText(status);
+                    }
+
+                    @Override
+                    public void onDirectionFailure(Throwable t) {
+
+                    }
+                });
+
     }
 }
